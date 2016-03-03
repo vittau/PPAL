8b1aabd8958b5ae3ca7dfe50360601f5b8094abd
vittau <vitor.p.mach@gmail.com>
2016-03-03 11:32:31 -0300

    Integration test for announcements.

--- MC/test/parser/BasicXMLPPALParserTest.java
+++ MC/test/parser/BasicXMLPPALParserTest.java
@@ -1,12 +1,15 @@
 package parser;
 
 import controller.SimulationState;
-import model.evaluable.BasicEvaluationFunction;
-import model.evaluable.BasicProposition;
-import model.evaluable.EvaluationFunction;
-import model.evaluable.Proposition;
+import model.evaluable.*;
+import model.societies.BasicAnnouncement;
+import model.societies.Group;
+import model.societies.Population;
+import model.societies.Society;
 import model.structures.BasicState;
 import model.structures.State;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.io.File;
@@ -19,35 +22,182 @@ import static org.testng.Assert.*;
  */
 public class BasicXMLPPALParserTest {
 
-    @Test
-    public void ppalExampleTest() {
-        final EvaluationFunction evf = new BasicEvaluationFunction();
+	private static final Double DELTA = 0.001;
 
-        PPALParser parser = new BasicXMLPPALParser(evf);
-        try {
-            assertTrue(parser.isDocumentValid(new File("./Examples./PPALExample.xml")), "The document is invalid.");
-        } catch (IOException e) {
-            fail("Failed to read the document.");
-        }
+	private static final EvaluationFunction EVF = new BasicEvaluationFunction();
+	private SimulationState simulationState = null;
+
+	@BeforeMethod
+	public void beforeTest() {
 
 
-        SimulationState simulationState = null;
-        try {
-            simulationState = parser.parseDocument(new File("./Examples/PPALExample.xml"));
-        } catch (IOException e) {
-            fail("Failed to read the document.");
-        }
+		PPALParser parser = new BasicXMLPPALParser(EVF);
+		try {
+			File file = new File("../Examples/PPALExample.xml");
+			assertTrue(parser.isDocumentValid(file), "The document is invalid.");
+		} catch (IOException e) {
+			fail("Failed to read the document.");
+		}
 
-        State realState = simulationState.getRealState();
+		try {
+			File file = new File("../Examples/PPALExample.xml");
+			simulationState = parser.parseDocument(file);
+		} catch (IOException e) {
+			fail("Failed to read the document.");
+		}
+	}
 
-        Proposition ah0 = new BasicProposition("a.h0", evf);
-        Proposition bh1 = new BasicProposition("b.h1", evf);
-        Proposition ch2 = new BasicProposition("c.h2", evf);
-        State expectedRealState = new BasicState(ah0, bh1, ch2);
+	@Test
+	public void realStateTest() {
 
-        assertEquals(realState, expectedRealState);
+		State realState = simulationState.getRealState();
 
-        //TODO: Implement the rest of this test.
-        fail("Implement me.");
-    }
+		Proposition ah0 = new BasicProposition("ah0", EVF);
+		Proposition bh1 = new BasicProposition("bh1", EVF);
+		Proposition ch2 = new BasicProposition("ch2", EVF);
+		State expectedRealState = new BasicState(ah0, bh1, ch2);
+
+		assertEquals(realState, expectedRealState);
+	}
+
+	@Test
+	public void AnnouncementResultingSizesTest() {
+		//Announcing bh1 to a with ratio 0.7
+		Proposition pre = new BasicProposition("bh1", EVF);
+		Population a = (Population) simulationState.getSociety("a");
+		Double ratio = 0.7;
+
+		Group newGroup = BasicAnnouncement.announce(a, pre, ratio);
+
+		String[] socMparents = "a".split("\\.");
+		if (socMparents.length == 1) {
+			simulationState.removeSociety(a);
+			simulationState.insertSociety(newGroup);
+		} else {
+			Society socMparent = simulationState.getSociety(socMparents[socMparents.length - 2]);
+			socMparent.getSocieties().remove(a);
+			socMparent.getSocieties().add(newGroup);
+		}
+
+		Society a_new = simulationState.getSociety("a.a_n");
+		Society a_old = simulationState.getSociety("a.a_o");
+		assertEquals(a_old.getSize(), 0.9, DELTA); //Part that received the announcement.
+		assertEquals(a_new.getSize(), 2.1, DELTA); //Part that did not receive the announcement.
+	}
+
+	@Test
+	public void NewNotKnowsArbitraryPropTest() {
+
+		State realState = simulationState.getRealState();
+
+		//Announcing bh1 to a with ratio 0.7
+		Proposition pre = new BasicProposition("bh1", EVF);
+		Population a = (Population) simulationState.getSociety("a");
+		Double ratio = 0.7;
+
+		Group newGroup = BasicAnnouncement.announce(a, pre, ratio);
+
+		simulationState.removeSociety(a);
+		simulationState.insertSociety(newGroup);
+
+		Society a_new = simulationState.getSociety("a.a_n");
+
+		//Checking if the new society in fact does not know an arbitrary proposition.
+		BasicKnowledgeOperator bko = new BasicKnowledgeOperator(a_new.getSocietyModel(), new BasicProposition("unknown", EVF));
+		assertEquals(bko.eval(a_new, realState), 0.0, DELTA);
+	}
+
+	@Test
+	public void NewKnowsAnnouncedPropTest() {
+
+		State realState = simulationState.getRealState();
+
+		Proposition bh1 = new BasicProposition("bh1", EVF);
+
+		//Announcing bh1 to a with ratio 0.7
+		Proposition pre = new BasicProposition("bh1", EVF);
+		Population a = (Population) simulationState.getSociety("a");
+		Double ratio = 0.7;
+
+		Group newGroup = BasicAnnouncement.announce(a, pre, ratio);
+
+		simulationState.removeSociety(a);
+		simulationState.insertSociety(newGroup);
+
+		Society a_new = simulationState.getSociety("a.a_n");
+
+		//Checking if the new society knows the announced proposition.
+		BasicKnowledgeOperator bko2 = new BasicKnowledgeOperator(a_new.getSocietyModel(), bh1);
+		assertEquals(bko2.eval(a_new, realState), 1.0, DELTA);
+	}
+
+	@Test
+	public void NewInferPropTest() {
+
+		State realState = simulationState.getRealState();
+
+		Proposition ch2 = new BasicProposition("ch2", EVF);
+
+		//Announcing bh1 to a with ratio 0.7
+		Proposition pre = new BasicProposition("bh1", EVF);
+		Population a = (Population) simulationState.getSociety("a");
+		Double ratio = 0.7;
+
+		Group newGroup = BasicAnnouncement.announce(a, pre, ratio);
+
+		simulationState.removeSociety(a);
+		simulationState.insertSociety(newGroup);
+
+		Society a_new = simulationState.getSociety("a.a_n");
+
+		//Checking if the new society can infer ch2 from the announcement.
+		BasicKnowledgeOperator bko3 = new BasicKnowledgeOperator(a_new.getSocietyModel(), ch2);
+		assertEquals(bko3.eval(a_new, realState), 1.0, DELTA);
+	}
+
+	@Test
+	public void OldNotKnowAnnouncedPropTest() {
+
+		State realState = simulationState.getRealState();
+
+		Proposition bh1 = new BasicProposition("bh1", EVF);
+
+		//Announcing bh1 to a with ratio 0.7
+		Proposition pre = new BasicProposition("bh1", EVF);
+		Population a = (Population) simulationState.getSociety("a");
+		Double ratio = 0.7;
+
+		Group newGroup = BasicAnnouncement.announce(a, pre, ratio);
+
+		simulationState.removeSociety(a);
+		simulationState.insertSociety(newGroup);
+
+		Society a_old = simulationState.getSociety("a.a_o");
+
+		//Checking if the old society does not know the announced proposition.
+		BasicKnowledgeOperator bko4 = new BasicKnowledgeOperator(a_old.getSocietyModel(), bh1);
+		assertEquals(bko4.eval(a_old, realState), 0.0, DELTA);
+	}
+
+	@Test
+	public void GroupKnowAnnouncedRatioTest() {
+
+		State realState = simulationState.getRealState();
+
+		Proposition bh1 = new BasicProposition("bh1", EVF);
+
+		//Announcing bh1 to a with ratio 0.7
+		Proposition pre = new BasicProposition("bh1", EVF);
+		Population a = (Population) simulationState.getSociety("a");
+		Double ratio = 0.7;
+
+		Group newGroup = BasicAnnouncement.announce(a, pre, ratio);
+
+		simulationState.removeSociety(a);
+		simulationState.insertSociety(newGroup);
+
+		//Checking if the group knows the announced proposition with the announced ratio.
+		BasicKnowledgeOperator bko5 = new BasicKnowledgeOperator(a.getSocietyModel(), bh1);
+		assertEquals(bko5.eval(a, realState), 0.7, DELTA);
+	}
 }
