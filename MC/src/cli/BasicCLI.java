0b4bfd6dfcc11397d1c37106040c6c5d07ebb15f
Vitor Machado <vitor.p.mach@gmail.com>
2016-04-12 18:55:51 -0300

    Fixed dependencies

--- MC/src/cli/BasicCLI.java
+++ MC/src/cli/BasicCLI.java
@@ -29,9 +29,10 @@ public class BasicCLI {
 
 	private static final String CONSOLE_NAME = "ppalmc";
 	private static final String VERSION = "0.2.1";
+	private static final String VERSION_CODENAME = "Second Flight";
 	private static final String HINT_TEXT =
 			"PPAL MODEL CHECKER\n" +
-			VERSION + " (Second Flight) - by Vitor Machado (vittau.github.io)\n" +
+			VERSION + " (" + VERSION_CODENAME + ") - by Vitor Machado (vittau.github.io)\n" +
 			"Enter \"?list\" for a list of available commands. For detailed info enter \"?help COMMAND_NAME\". Enter \"exit\" to quit.";
 
 	private final EvaluationFunction evf = new BasicEvaluationFunction();
